d87b31eda4bd0d482f87868fd46236864c1f8e50
netto <euclydes.young@bluetab.net>
2019-03-19 22:47:25 +0100

    feat: [TD-1571] Elixirs logger can be configured with env variable

--- config/config.exs
+++ config/config.exs
@@ -33,8 +33,11 @@ config :td_df, TdDf.Repo,
   pool_size: 10
 
 # Configures Elixir's Logger
+# set EX_LOGGER_FORMAT environment variable to override Elixir's Logger format
+# (without the 'end of line' character)
+# EX_LOGGER_FORMAT='$date $time [$level] $message'
 config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
+  format: (System.get_env("EX_LOGGER_FORMAT") || "$time $metadata[$level] $message") <> "\n",
   metadata: [:request_id]
 
 config :td_df, TdDf.Auth.Guardian,
