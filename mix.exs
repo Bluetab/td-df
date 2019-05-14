c93b968cce12ee482229b802d2fc3260a9ab5184
netto <euclydes.young@bluetab.net>
2019-05-14 10:11:05 +0200

    chore: release 2.19.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TdDf.Mixfile do
       app: :td_df,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "2.16.0-local"
+          nil -> "2.19.0-local"
           v -> v
         end,
       elixir: "~> 1.6",
